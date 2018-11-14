0ce0b653bb776cdbcb67ecf9e073b7d8faeecab1
Kevin Doody <kdoody@sysco.ie>
2018-11-14 12:17:14 +0000

    code change

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            }
         }
     }
 }
